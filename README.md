20d5cd450a88b8dee77f1c27ab84155776f69b6c
Jeff Dean <jeff@galvanize.it>
2014-11-24 23:15:13 -0700

    prepared the exercise

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-## Exercise
+## Bootstrapify some forms
+
+Use this app to practice adding bootstrap styles to real forms.
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141125061022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "people", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.date     "date_of_birth"
+    t.text     "bio"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
